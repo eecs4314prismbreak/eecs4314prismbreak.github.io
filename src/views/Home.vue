5d0607ef9a83ea40374c9905d7fac5d51b7f42f9
jamiedishy <jamiedishy@gmail.com>
2020-09-17 21:08:28 -0400

    IDs - all added

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,18 +4,23 @@
       <div class="col-md-12">
         <header>
           <h1>EECS 4314 - Team Prism Break</h1>
-          <hr class="mb-2 header-rule">
+          <hr class="mb-2 header-rule" />
           <p>
-            These assignments are intended to provide students with an appreciation for creating and evolving the software design and architecture in the context of a large-scale software system. This document outlines Prism Break's project and a detailed architecture for its software system. The architecture will be updated throughout the term according to each assignment.
+            These assignments are intended to provide students with an
+            appreciation for creating and evolving the software design and
+            architecture in the context of a large-scale software system. This
+            document outlines Prism Break's project and a detailed architecture
+            for its software system. The architecture will be updated throughout
+            the term according to each assignment.
           </p>
         </header>
-        <hr class="my-2">
+        <hr class="my-2" />
         <rbc-card class="mt-3">
           <h2>Members</h2>
-          <ul class="list-highlight">
+          <ul class="mt-2 list-highlight">
             <li class="mb-1">Jinho Hwang - 215240559</li>
             <li class="mb-1">Jamie Dishy - 212904041</li>
-            <li class="mb-1">Kevin Banh</li>
+            <li class="mb-1">Kevin Banh - 213565676</li>
             <li class="mb-1">Allen Kaplan - 215494925</li>
             <li class="mb-1">Amer Alshoghri - 214992291</li>
           </ul>
@@ -26,8 +31,8 @@
 </template>
 
 <script>
-import { Card } from 'rbc-wm-framework-vuejs/dist/wm/components';
-export default { 
+import { Card } from "rbc-wm-framework-vuejs/dist/wm/components";
+export default {
   name: "Home",
   components: {
     "rbc-card": Card
