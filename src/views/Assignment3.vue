a77222625f166ee1ffd1b92cac03a2124f1d4ce9
jamiedishy <jamiedishy@gmail.com>
2020-11-15 13:16:49 -0500

    Assignment 3 - report and slides added

--- src/views/Assignment3.vue
+++ src/views/Assignment3.vue
@@ -6,7 +6,12 @@
           <h1>Assignment 3</h1>
           <hr class="mb-2 header-rule" />
           <p>
-            Coming soon.
+            See below for a report that covers the concrete architecture of
+            WheyPal and how it compares with that from Assignment 1. Diagrams
+            are presented in the report that clearly visualize the services
+            implemented and their interactions. Prism Break uses
+            microservices/containers for deployment and implements 2 use-cases
+            in Assignment 3.
           </p>
         </header>
         <hr class="my-2" />
@@ -14,10 +19,18 @@
           <h2>Submissions</h2>
           <ul class="list-highlight">
             <li class="mb-1">
-              <a href="#" class="file-ppt">Presentation Slides</a>
+              <a
+                href="https://docs.google.com/presentation/d/1QpSmZsTaWOtLMnFNU2yxeTc0r4kAZCuACFGKJWXUm10/edit?usp=sharing"
+                class="file-ppt"
+                >Presentation Slides</a
+              >
             </li>
             <li class="mb-1">
-              <a href="#" class="file-pdf">Report</a>
+              <a
+                href="https://docs.google.com/document/d/1_Ldkhei_ynnhxUBOV5twDMLpr9evq6S9aXfqcJo3tBg/edit?usp=sharing"
+                class="file-pdf"
+                >Report</a
+              >
             </li>
           </ul>
         </rbc-card>
@@ -31,8 +44,8 @@ import { Card } from "rbc-wm-framework-vuejs/dist/wm/components";
 export default {
   name: "Assignment3",
   components: {
-    "rbc-card": Card
-  }
+    "rbc-card": Card,
+  },
 };
 </script>
 
